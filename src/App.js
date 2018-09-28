ec3831ad66cd14c3ff4dd3078346f5a8be94e35b
finkegabriel <finkegabriel@gmail.com>
2018-09-28 04:54:25 -0700

    First

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+//import logo from './rovercircle.svg';
+import './App.css';
+import logos from './line.png';
+import { EventEmitter } from 'events';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.move = this.move.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+    move(event) {
+    //console.log(event.target.value);
+    console.log(this.state.value);
+    event.preventDefault();
+
+    //this.setState({deg: 'useless placeholder'});
+    //this.state.value===i;
+   
+    
+    
+    
+    /*    document.getElementById('h3').onChange = function(){
+          var div = document.getElementById('degcenter'),
+                      deg = rotated ? 0:66;
+                     }*/
+
+    //console.log(this.state.value);
+    /*if(this.state.value===''){
+      console.log('accepted');
+    }else{
+      console.log('error');
+    }*/
+  }
+
+    /*divStyle ={
+
+    position: 'relative',
+    transform: 'rotate(0deg)',
+    transition: 'rotate 500ms linear'
+    
+  };*/
+
+
+  
+  render() {
+    
+    return (
+      
+      <div className="App">
+      
+        <center><h2>Mars - Earth Hex COM</h2></center>
+        
+        <p>
+          <form onSubmit={this.move}>
+            <p>
+              <textarea value={this.state.value}></textarea>
+            </p>
+            <input type="text" name="message" placeholder="Message" value={this.state.value} onChange={this.handleChange} />
+
+            <p>
+            <input type="submit" value="Send" />
+            </p>
+
+          </form>
+        </p>
+
+
+        
+        <header className="App-header">
+
+          <div class='circle-container'>
+       
+            <a class='degcenter' style={this.divStyle}><h3>===============></h3></a>
+            <a class='deg0'><h3>0</h3></a>
+            <a class='deg21'><h3>1</h3></a>
+            <a class='deg42'><h3>2</h3></a>
+            <a class='deg63'><h3>3</h3></a>
+            <a class='deg84'><h3>4</h3></a>
+            <a class='deg105'><h3>5</h3></a>
+            <a class='deg127'><h3>6</h3></a>
+            <a class='deg148'><h3>7</h3></a>
+            <a class='deg169'><h3>8</h3></a>
+            <a class='deg190'><h3>9</h3></a>
+            <a class='deg211'><h3>A</h3></a>
+            <a class='deg232'><h3>B</h3></a>
+            <a class='deg254'><h3>C</h3></a>
+            <a class='deg275'><h3>D</h3></a>
+            <a class='deg296'><h3>E</h3></a>
+            <a class='deg317'><h3>F</h3></a>
+            <a class='deg338'><h3>?</h3></a>
+
+
+
+
+          </div>
+
+        </header>
+
+      </div>
+
+
+    );
+  }
+}
+
+
+
+
+export default App;
---
+++ src/move.js
@@ -0,0 +1,9 @@
+$(".all-slides").sortable({
+  
+  axis: "y",
+  revert: true,
+  scroll: false,
+  placeholder: "sortable-placeholder",
+  cursor: "move"
+
+});
